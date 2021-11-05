f81b416449353763f2a71a731ae54afddb5af40f
UlongChaS2 <jyujung2@gmail.com>
2021-11-05 18:17:17 +0900

    Chore: basic practice

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>test</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: 'app',
+};
 </script>
 
 <style>
-#app {
+#test {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
