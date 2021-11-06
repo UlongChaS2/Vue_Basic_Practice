496f60617353febe33776da234c6ec407924f470
UlongChaS2 <jyujung2@gmail.com>
2021-11-06 21:36:49 +0900

    Add: addEventListener

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div :style="customStyle">{{ check1 }}</div>
   <div>{{ check2 }}</div>
-  <button>버튼</button>
   <div v-for="(arr, idx) in products" :key="arr">{{ arr }}{{ idx }}</div>
+  <button v-on:click="count++">숫자 추가</button>
+  <button @click="count++">숫자 추가</button>
+  <button @mouseover="count++">숫자 추가</button>
+  <span>count: {{ count }}</span>
 </template>
 
 <script>
@@ -14,6 +17,7 @@ export default {
       check2: '확인',
       customStyle: 'color: blue',
       products: ['일반 Array', '일반 Array', '일반 Array'],
+      count: 0,
     };
   },
 };
